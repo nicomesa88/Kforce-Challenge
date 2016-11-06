eddcf992765fcfd667dedf70264d3776f838daad
Nico Mesa <nicomesa88@gmail.com>
2016-11-06 13:52:36 -0600

    added search artist collection to models.js

--- src/scripts/models/models.js
+++ src/scripts/models/models.js
@@ -1,2 +1,10 @@
 import Backbone from 'backbone'
 import $ from 'jquery'
+import {app_name} from '../app'
+
+const SearchArtistCollection = Backbone.Collection.extend({
+	url: 'https://api.spotify.com/v1/search/',
+	parse: function(rawJSONP){
+		return rawJSONP.artists
+	}
+})
