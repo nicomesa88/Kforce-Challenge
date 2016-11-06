b2a2d366e545038fe2cc174b94b3aee06829dd7f
Nico Mesa <nicomesa88@gmail.com>
2016-11-06 14:09:51 -0600

    added suggested Albums collection to models.js

--- src/scripts/models/models.js
+++ src/scripts/models/models.js
@@ -2,7 +2,7 @@ import Backbone from 'backbone'
 import $ from 'jquery'
 import {app_name} from '../app'
 
-const SearchArtistCollection = Backbone.Collection.extend({
+const SearchCollection = Backbone.Collection.extend({
 	url: 'https://api.spotify.com/v1/search/',
 	parse: function(rawJSONP){
 		return rawJSONP.artists
@@ -13,6 +13,12 @@ const SerchArtistModel = Backbone.Model.extend({
 	url: 'https://api.spotify.com/v1/search/',
 	parse: function(rawJSONP){
 		return rawJSONP.artists
+	}
+})
+
+const SuggestedAlbumsCollection = Backbone.Collection.extend({
+	parse: function(rawJSONP){
+		return rawJSONP.items
 	}
 })
 
